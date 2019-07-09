33744e390258ee8f5ced8859ebe6a7e48b712f1b
NPD-UESPI <seu_email@email.com>
2019-07-09 13:56:57 -0300

    09.07.2019.651fg56

--- application/views/listar_medicos_view.php
+++ application/views/listar_medicos_view.php
@@ -37,7 +37,20 @@ $this->load->view('commons/imports_cabecalho');
 	href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
 	integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU"
 	crossorigin="anonymous">
+<script>
+window.onload = function() {
+	fechaAlert();
+};
 
+function fechaAlert(){
+	setTimeout(function() {
+		console.log('ok');
+	    $(".alert").fadeTo(500, 0).slideUp(500, function(){
+	        $(this).remove(); 
+	    });
+	}, 4000);
+}
+</script>
 </head>
 <body>
 	<div id="wrapper">
